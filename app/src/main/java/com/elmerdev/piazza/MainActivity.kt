bd784cb56c0c8d2b3506105f0626839509fcf1fb
Elmer125 <elmermauricio05@hotmail.com>
2024-01-20 21:25:00 -0500

    Initial commit

---
+++ app/src/main/java/com/elmerdev/piazza/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.elmerdev.piazza
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}

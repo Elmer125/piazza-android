a8385d3a32f9d059e00dc3e1a8351a7f6f67eaeb
Elmer125 <elmermauricio05@hotmail.com>
2024-02-01 21:26:15 -0500

    Add Internet permission and enable local HTTP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
   namespace = "com.elmerdev.piazza"
-  compileSdk = 33
+  compileSdk = 34
 
   defaultConfig {
     applicationId = "com.elmerdev.piazza"
     minSdk = 24
-    targetSdk = 33
+    targetSdk = 34
     versionCode = 1
     versionName = "1.0"
 

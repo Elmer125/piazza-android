6dbdef0cca4dd11c34dc41242f4156117fe819d3
Elmer125 <elmermauricio05@hotmail.com>
2024-02-05 07:38:07 -0500

    Setup tabbed navigation

---
+++ app/src/main/java/com/elmerdev/piazza/Api.kt
@@ -0,0 +1,17 @@
+package com.elmerdev.piazza
+
+import dev.hotwire.turbo.BuildConfig
+
+class Api {
+  companion object {
+//    private val baseUrl = if(BuildConfig.DEBUG)
+//      "http://192.168.0.11:3000"
+//    else
+//      "https://piazza-web-3ps7.onrender.com"
+    private val baseUrl = "http://192.168.0.11:3000"
+
+    val rootUrl = "$baseUrl/"
+    val loginUrl = "$baseUrl/login"
+
+  }
+}
---
+++ app/src/main/java/com/elmerdev/piazza/WebFragment.kt
@@ -0,0 +1,7 @@
+package com.elmerdev.piazza
+
+import dev.hotwire.turbo.fragments.TurboWebFragment
+import dev.hotwire.turbo.nav.TurboNavGraphDestination
+
+@TurboNavGraphDestination(uri = "piazza://fragment/web")
+open class WebFragment : TurboWebFragment() {}
